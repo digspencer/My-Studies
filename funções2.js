d86db2a672240abbe2fcaefa816b65976e1e8779
digspencer <diegoiglan@hotmail.com>
2023-02-14 16:11:36 -0300

    funções2.js

---
+++ funções2.js
@@ -0,0 +1,15 @@
+// Armazenando uma função em uma variável
+const imprimirSoma = function (a, b) {console.log(a + b)}
+
+imprimirSoma (2, 3);
+
+// Armazenando uma função Arrow em uma variavel
+const soma = (a, b) => {
+    return a + b;
+}
+
+console.log(soma(2, 5));
+
+// Retorno implícito, forma abreviada
+const substracao = (a, b) => a - b; 
+console.log(substracao(10,5));
