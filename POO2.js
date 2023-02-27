9b6e283f310cfd73c82bc26d209a3bd5dc5b9f20
digspencer <diegoiglan@hotmail.com>
2023-02-27 15:19:41 -0300

    Adicionais mais arq

--- POO2.js
+++ POO2.js
@@ -1,46 +1,22 @@
-class Animal {
-    constructor(nome, idade, peso){
+class Funcionario{
+    constructor(nome, idade, profissao){
         this.nome = nome;
         this.idade = idade;
-        this.peso = peso;
-    }
-    latir(){
-    console.log(`${this.nome} made some noise!`)
-    }
-    falar(){
-    console.log(`${this.nome} repetiu o som que ouviu!`)
-}
-    rosnar(){
-        console.log(`${this.nome} emitiu um som que dá medo!`)
+        this.profissao = profissao;
     }
 }
 
-const dog = new Animal("Ted", 2, 5.5)
-dog.latir()
-
-const papagaio = new Animal("Pip", 3, 0.5)
-papagaio.falar()
-
-const tartaruga = new Animal("Ana", 32, 7.6)
-tartaruga.falar()
-
-class Mamiferos extends Animal{
-    constructor(nome, idade, peso){
-    super(nome, idade, peso);
+class dev extends Funcionario{
+    constructor(nome, idade, profissao){
+        super(nome, idade, profissao)
     }
-    mamar(){
-        console.log(`${this.nome} mamou hoje`)
+    daily(){
+        console.log(`${this.nome} participou do Daily!`)
     }
-    nadar(){
-        console.log(`${this.nome} nadou hoje`)
+    pointRegister(){
+        console.log(`${this.nome} registrou o ponto!`)
     }
 }
 
-const gatinho = new Mamiferos ("Tom", 6, 2.5)
-gatinho.mamar()
-
-const patinho = new Mamiferos("Sophia", 4, 4.4)
-patinho.nadar()
-
-const leao = new Mamiferos("Concil", 20, 50)
-leao.rosnar()
+const diego = new dev("Diego", 33, "desenvolvedor")
+diego.pointRegister()
